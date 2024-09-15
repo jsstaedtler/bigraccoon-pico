7abdffad759720c1ca1e6a129bbfbd35748d8079
jsstaedtler <jalbenphillips@gmail.com>
2024-09-15 18:14:24 -0400

    Update PicoPageViews.php

--- plugins/PicoPageViews/PicoPageViews.php
+++ plugins/PicoPageViews/PicoPageViews.php
@@ -13,7 +13,7 @@ class PicoPageViews extends AbstractPicoPlugin
     public function onSinglePageLoading($id, &$skipPage)
     {
         if ($id === '_stats') { $skipFile = true; }
-    }    
+    }
     
     public function onCurrentPageDiscovered(
         array &$currentPage = null,
@@ -23,14 +23,19 @@ class PicoPageViews extends AbstractPicoPlugin
         $currentPage = $this->getPico()->getCurrentPage();
         if ($currentPage !== null) {
 			
+			// Open the file as read/write without erasing its contents (creating it if it doesn't exist)
             $file = fopen($this->md, 'c+');
 
+			// Get an exclusive lock on the file; a shared lock for reading isn't appropriate, since another process could then read it before this one has updated it
             if (flock($file, LOCK_EX)) {
 				
                 $currentPageId = $this->getPico()->getCurrentPage()['id'];
+				
 				$this->loadStats($file);
                 $this->statsPageMeta['stats'][$currentPageId]++;
                 $this->saveStats($file);
+				
+				// Release the file lock
                 flock($file, LOCK_UN);
 				
             }
@@ -43,15 +48,17 @@ class PicoPageViews extends AbstractPicoPlugin
     private function loadStats($file)
     {
         $currentPageId = $this->pico->getCurrentPage()['id'];
-        // front matter YAML
+		
+        // Read the full contents of the stats file (but only if it has any contents)
 		$fileLength = filesize($this->md);
         $frontMatter = $fileLength > 0 ? fread($file, $fileLength) : '';
+		
+		// Turn the contents into an array of data
         $frontMatterArray = $this->getPico()->parseFileMeta($frontMatter, []);        
         $this->statsPageMeta = $frontMatterArray;
         $this->statsPageMeta['stats'] = $this->statsPageMeta['stats'] ?? [];
 		
-		error_log('Number of IDs loaded: ' . count($this->statsPageMeta['stats']));
-		
+		// If the current page isn't already in this array, then put it in
         if (!array_key_exists($currentPageId, $this->statsPageMeta['stats'])) {
             $this->statsPageMeta['stats'][$currentPageId] = 0;
         }                
@@ -59,10 +66,11 @@ class PicoPageViews extends AbstractPicoPlugin
     
     private function saveStats($file)
     {
-        // update the front matter
+        // Convert our array of stats into a string of YAML
         $frontMatterArray = array('stats' => $this->statsPageMeta['stats']);
         $yaml = "---\n" . Yaml::dump($frontMatterArray) . "---\n";
 		
+		// Overwrite the stats file with our new data
 		ftruncate ($file, 0);	// Empty the file
 		rewind($file);			// Return to position 0
         fwrite($file, $yaml);
