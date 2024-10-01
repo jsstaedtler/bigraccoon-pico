14153e2aec4e0b1691ad4efac0d475ee2a797703
jsstaedtler <jalbenphillips@gmail.com>
2024-09-30 21:57:56 -0400

    PicoPageViews now ignores IP addresses provided in config.yml

--- plugins/PicoPageViews/PicoPageViews.php
+++ plugins/PicoPageViews/PicoPageViews.php
@@ -33,44 +33,70 @@ class PicoPageViews extends AbstractPicoPlugin
         $this->template = null;
         if (isset($config['PicoPageViews']['template']))
             $this->template = $config['PicoPageViews']['template'];
+		if (isset($config['PicoPageViews']['make_hidden']))
             $this->make_hidden = $config['PicoPageViews']['make_hidden'];
+		if (isset($config['PicoPageViews']['ignore_addresses']))
+            $this->ignore_list = $config['PicoPageViews']['ignore_addresses'];
     }
-	
+
 	
     public function onCurrentPageDiscovered(
         array &$currentPage = null,
         array &$previousPage = null,
         array &$nextPage = null
     ) {
-		date_default_timezone_set('America/Toronto');
-		$this->currentDate = date('Y-m-d');
-		$this->currentTime = date('H:i');
-		$this->md = $this->statsDir . '/stats' . $this->currentDate . '.md';
-		
-        $currentPage = $this->getPico()->getCurrentPage();
-		if ($currentPage !== null) {
-			$currentPageId = $currentPage['id'];
+		// Don't continue if the user is visiting from an IP address that we're ignoring
+		if ($this->valid_address()) {
+			
+			date_default_timezone_set('America/Toronto');
+			$this->currentDate = date('Y-m-d');
+			$this->currentTime = date('H:i');
+			$this->md = $this->statsDir . '/stats' . $this->currentDate . '.md';
+			
+			$currentPage = $this->getPico()->getCurrentPage();
+			if ($currentPage !== null) {
+				$currentPageId = $currentPage['id'];
 
-			if ($currentPageId !== null && strpos($currentPageId, 'stats') !== 0) {		// Don't count stats pages themselves
-				
-				// Open the stats file as read/write without erasing its contents (creating it if it doesn't exist)
-				$file = fopen($this->md, 'c+');
-
-				// Get an exclusive lock on the file; a shared lock for reading isn't appropriate, since another process could then read it before this one has updated it
-				if (flock($file, LOCK_EX)) {
-					$this->loadStats($file);
-					$this->statsPageMeta['stats'][$currentPageId]++;
-					$this->saveStats($file);
+				if ($currentPageId !== null && strpos($currentPageId, 'stats') !== 0) {		// Don't count stats pages themselves
 					
-					// Release the file lock
-					flock($file, LOCK_UN);
-				}
-				
-				fclose($file);
+					// Open the stats file as read/write without erasing its contents (creating it if it doesn't exist)
+					$file = fopen($this->md, 'c+');
 
+					// Get an exclusive lock on the file; a shared lock for reading isn't appropriate, since another process could then read it before this one has updated it
+					if (flock($file, LOCK_EX)) {
+						$this->loadStats($file);
+						$this->statsPageMeta['stats'][$currentPageId]++;
+						$this->saveStats($file);
+						
+						// Release the file lock
+						flock($file, LOCK_UN);
+					}
+					
+					fclose($file);
+
+				}
 			}
+		
 		}
     }
+	
+	
+	private function valid_address()
+	{
+		if (
+			array_key_exists('REMOTE_ADDR',$_SERVER)		// This global should always be set, but let's not take chances
+			&& !empty($_SERVER['REMOTE_ADDR'])				// There should always be a value in there, but again, let's be safe
+			&& isset($this->ignore_list) 					// Does an ignore list exist in the site config
+			&& is_array($this->ignore_list)					// Is it an array of values as expected
+		) {
+			// If the user's address is in the ignore list, we state that it should not be counted
+			if (in_array($_SERVER['REMOTE_ADDR'], $this->ignore_list))
+				return false;
+		}
+		
+		// In all other cases, count the visit from this address
+		return true;
+	}
 
     private function loadStats($file)
     {
